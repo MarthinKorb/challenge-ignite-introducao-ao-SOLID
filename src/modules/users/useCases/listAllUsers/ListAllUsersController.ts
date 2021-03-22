d8ffa8858f34e6f32fdb91f6f349f301d585f640
Marthin Korb <marthinkorb@gmail.com>
2021-03-22 18:06:45 -0300

    Adding Swagger

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,12 +6,9 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // const { user_id } = request.params;
     const user_id = request.headers.user_id as string;
-
     try {
       const users = this.listAllUsersUseCase.execute({ user_id });
-
       return response.status(200).json(users);
     } catch (error) {
       return response.status(400).json({ error });
